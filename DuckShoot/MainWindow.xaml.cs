c3b500326d77677e7e0c26334ced4500aa17894b
Paula Kochańska <261681@pw.edu.pl>
2017-10-23 13:56:33 +0200

    jfbsdfdf

--- DuckShoot/MainWindow.xaml.cs
+++ DuckShoot/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace DuckShoot
     public partial class MainWindow : Window
     {
         int tick = -4;
+        int count = 0;
+        int countSuccess = 0;
 
         public MainWindow()
         {
@@ -40,13 +42,27 @@ namespace DuckShoot
             {
                 ImgDuck1.RenderTransformOrigin = new Point(0.5, 0.5);
                 ScaleTransform flipTrans = new ScaleTransform();
-                flipTrans.ScaleX = -1; // or 1
-                                       //flipTrns.ScaleY = -1;
+                flipTrans.ScaleX = -1;
                 ImgDuck1.RenderTransform = flipTrans;
                 tick = -tick;
             }
-
-            ImgDuck1.Margin = new Thickness(ImgDuck1.Margin.Left + tick, ImgDuck1.Margin.Top, 0, 0);            //ImgDuck2.Margin = new Thickness(ImgDuck2.Margin.Left + 2, ImgDuck2.Margin.Top, 0, 0);
+            ImgDuck1.Margin = new Thickness(ImgDuck1.Margin.Left + tick, ImgDuck1.Margin.Top, 0, 0);            if (ImgDuck2.Margin.Left <= 0)
+            {
+                ImgDuck2.RenderTransformOrigin = new Point(0.5, 0.5);
+                ScaleTransform flipTrans = new ScaleTransform();
+                flipTrans.ScaleX = -1; 
+                ImgDuck2.RenderTransform = flipTrans;
+                tick = -tick;
+            }
+            ImgDuck2.Margin = new Thickness(ImgDuck2.Margin.Left + tick, ImgDuck2.Margin.Top, 0, 0);            if (ImgDuck3.Margin.Left <= 0)
+            {
+                ImgDuck3.RenderTransformOrigin = new Point(0.5, 0.5);
+                ScaleTransform flipTrans = new ScaleTransform();
+                flipTrans.ScaleX = -1;
+                ImgDuck3.RenderTransform = flipTrans;
+                tick = -tick;
+            }
+            ImgDuck3.Margin = new Thickness(ImgDuck3.Margin.Left + tick, ImgDuck3.Margin.Top, 0, 0);            //ImgDuck2.Margin = new Thickness(ImgDuck2.Margin.Left + 2, ImgDuck2.Margin.Top, 0, 0);
             //ImgDuck3.Margin = new Thickness(ImgDuck3.Margin.Left + 2, ImgDuck3.Margin.Top, 0, 0);
         }
 
@@ -54,6 +70,27 @@ namespace DuckShoot
         {
             var point = e.GetPosition(WindowDuck);
             ImgTarget.Margin = new Thickness(point.X - ImgTarget.Width/2, point.Y - ImgTarget.Height / 2, 0, 0);
+        }
+
+        private void ImgDuck1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            countSuccess += 1;
+            LblCounter.Content = countSuccess;
+            ImgDuck1.Visibility = Visibility.Hidden;
+        }
+
+        private void ImgDuck2_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            countSuccess += 1;
+            LblCounter.Content = countSuccess;
+            ImgDuck2.Visibility = Visibility.Hidden;
+        }
+
+        private void ImgDuck3_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            countSuccess += 1;
+            LblCounter.Content = countSuccess;
+            ImgDuck3.Visibility = Visibility.Hidden;
         }
     }
 }
